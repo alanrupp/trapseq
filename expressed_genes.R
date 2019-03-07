3b82c2f978e5b548df1ba53075c2786f58ae4b78
Alan Rupp <alan.rupp@gmail.com>
2019-03-06 21:29:47 -0500

    updated expressed_genes

--- expressed_genes.R
+++ expressed_genes.R
@@ -1,5 +1,4 @@
 expressed_genes <- function(counts, threshold = 1, samples = 3) {
-  expressed <-
     counts %>%
     apply(., 2, function(x) x / sum(x) * 10^6) %>%
     apply(., 2, function(x) x >= threshold) %>%
